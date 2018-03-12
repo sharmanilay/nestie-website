13345b8eccadfac845935c58d399f77e12e7f337
sharmnilay <nilaysharma642@gmail.com>
2018-03-12 19:23:03 +0530

    sdjfsdf

--- js/main.js
+++ js/main.js
@@ -33,7 +33,25 @@ $(document).ready(function(){
 	);
 	wow.init();
 
+
+
 });
+$('.carousel[data-type="multi"] .item').each(function() {
+				var next = $(this).next();
+				if (!next.length) {
+					next = $(this).siblings(':first');
+				}
+				next.children(':first-child').clone().appendTo($(this));
+
+				for (var i = 0; i < 2; i++) {
+					next = next.next();
+					if (!next.length) {
+						next = $(this).siblings(':first');
+					}
+
+					next.children(':first-child').clone().appendTo($(this));
+				}
+			});
 
 function castParallax() {
 
